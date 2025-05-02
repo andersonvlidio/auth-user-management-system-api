3328f31519586de57db66ff87569fe29d33c0b9a
andersonvlidio <andersonvlidio@gmail.com>
2025-05-02 15:11:35 -0300

    chore: config db

---
+++ prisma/migrations/20250502173140_init/migration.sql
@@ -0,0 +1,24 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'VISUALIZADOR');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "cpf" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "birthDate" TIMESTAMP(3) NOT NULL,
+    "accessDeadline" TIMESTAMP(3) NOT NULL,
+    "role" "Role" NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_cpf_key" ON "User"("cpf");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
