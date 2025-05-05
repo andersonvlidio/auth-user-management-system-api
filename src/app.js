22cde2be78b6089524a472b2c11d4b5e92a96029
andersonvlidio <andersonvlidio@gmail.com>
2025-05-04 22:06:07 -0300

    chore: config cors

--- src/app.js
+++ src/app.js
@@ -2,8 +2,22 @@ const express = require('express');
 const app = express();
 const userRoutes = require('./routes/userRoutes');
 const authRoutes = require('./routes/authRoutes');
+const cors = require('cors');
+
 
 app.use(express.json());
+
+const allowedOrigins = ['https://auth-user-management-system.vercel.app/', 'http://localhost:5173'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
 
 app.get('/', (req, res) => {
   res.send('API is running...');
